aa81079ba5bd85abf664f40c4532f2f12cecc1f7
Ekaterina Kilina <kilina2018@bk.ru>
2023-09-12 14:07:36 +0600

    update gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+program
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1> <filepath2>');
+
+program.parse(process.argv);

56b4df711f30334764895b197cfe91f698dbbed2
Ekaterina Kilina <kilina2018@bk.ru>
2023-11-11 16:54:30 +0600

    change responsibility between buildDiffTree and gendiff

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,9 +2,32 @@ import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import parse from './parsers/parser.js';
-import formatSelection from '../formatters/index.js';
+import formatSelection from './formatters/index.js';
 
 const isAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
+
+const buildDiffTree = (obj1, obj2) => {
+  const keys = _.union(_.keys(obj1), _.keys(obj2));
+  const threePart = (key) => {
+    if (!_.has(obj1, key)) {
+      return { key, value: obj2[key], status: 'added' };
+    }
+    if (!_.has(obj2, key)) {
+      return { key, value: obj1[key], status: 'removed' };
+    }
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      return { key, children: buildDiffTree(obj1[key], obj2[key]), status: 'nested' };
+    }
+    if (obj1[key] !== obj2[key]) {
+      return {
+        key, oldValue: obj1[key], newValue: obj2[key], status: 'updated',
+      };
+    }
+    return { key, value: obj1[key], status: 'unchanged' };
+  };
+  const diff = keys.flatMap((key) => threePart(key));
+  return diff;
+};
 
 const gendiff = (path1, path2, format = 'stylish') => {
   const absolutePath1 = isAbsolutePath(path1);
@@ -17,26 +40,11 @@ const gendiff = (path1, path2, format = 'stylish') => {
 
   const parsedData1 = parse(fileContent1, ext1);
   const parsedData2 = parse(fileContent2, ext2);
-}
 
-const buildDiff = (obj1, obj2, format = 'stylish') => {
-  const keys = _.union(_.keys(obj1), _.keys(obj2));
-  const threePart = (key) => {
-    if (!_.has(obj1, key)) {
-      return { key, value: obj2[key], status: 'added' };
-    } else if (!_.has(obj2, key)) {
-      return { key, value: obj1[key], status: 'removed' };
-    } else if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      return { key, children: buildDiff(obj1[key], obj2[key]), status: 'nested' };
-    } else if (obj1[key] !== obj2[key]) {
-      return {
-        key, oldValue: obj1[key], newValue: obj2[key], status: 'updated',
-      };
-    }
-    return { key, value: obj1[key], status: 'unchanged' };
-  };
-  const diff = keys.flatMap((key) => threePart(key));
+  const diff = buildDiffTree(parsedData1, parsedData2);
+
   const outputFormat = formatSelection(format);
   return outputFormat(diff);
 };
-export default buildDiff;
+
+export default gendiff;

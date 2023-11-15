396b0adab061e635bfe5fbcdc0981e269fbdb34b
Ekaterina Kilina <kilina2018@bk.ru>
2023-11-15 19:55:44 +0600

    plain formatter refactoring

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -5,12 +5,15 @@ const plainOutput = (array, keyPath = '') => {
     const newKeyPath = keyPath === '' ? `${obj.key}` : `${keyPath}.${obj.key}`;
     if (obj.status === 'nested') {
       return plainOutput(obj.children, newKeyPath);
-    } else if (obj.status === 'added') {
+    }
+    if (obj.status === 'added') {
       const value = _.isObject(obj.value) ? '[complex value]' : obj.value;
       return `Property '${newKeyPath}' was added with value: ${value}"`;
-    } else if (obj.status === 'removed') {
+    }
+    if (obj.status === 'removed') {
       return `Property '${newKeyPath}' was removed`;
-    } else if (obj.status === 'updated') {
+    }
+    if (obj.status === 'updated') {
       const val1 = _.isObject(obj.oldValue) ? '[complex value]' : obj.oldValue;
       const val2 = _.isObject(obj.newValue) ? '[complex value]' : obj.newValue;
       return `Property '${newKeyPath}' was updated. From '${val1}' to '${val2}'`;

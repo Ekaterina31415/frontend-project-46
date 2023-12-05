ad2569633766a83fda9c7ea787844f08298e5e94
Ekaterina Kilina <kilina2018@bk.ru>
2023-12-05 16:34:01 +0600

    revrite stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,59 +1,49 @@
 import _ from 'lodash';
 
-const getIndentSize = (depth) => depth * 4 - 2;
-const getIndentString = (indentSize) => ' '.repeat(indentSize);
+const spacesCount = 4;
+const getIndent = (depth) => ' '.repeat(depth * spacesCount - 2);
 
-const stringify = (value, margin) => {
-  const iter = (currentValue, depth) => {
-    if (!_.isObject(currentValue)) {
-      return `${currentValue}`;
-    }
-
-    const indentSize = getIndentSize(depth) + margin;
-    const currentIndent = getIndentString(indentSize);
-    const bracketIndent = getIndentString(indentSize - 2);
-    const lines = Object
-      .entries(currentValue)
-      .map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
-
-    return [
-      '{',
-      ...lines,
-      `${bracketIndent}}`,
-    ].join('\n');
-  };
-
-  return iter(value, margin);
+const signs = {
+  added: '+ ',
+  removed: '- ',
+  unchanged: '  ',
 };
 
-const stylishOutput = (value) => {
-  const iter = (currentValue, depth) => {
-    const indentSize = getIndentSize(depth);
-    const indent = getIndentString(indentSize);
-    const bracketIndent = getIndentString(indentSize - 2);
-    const lines = currentValue.map((node) => {
-      if (node.status === 'added') {
-        return `${indent}+ ${node.key}: ${stringify(node.value, indentSize)}`;
-      }
-      if (node.status === 'removed') {
-        return `${indent}- ${node.key}: ${stringify(node.value, indentSize)}`;
-      }
-      if (node.status === 'unchanged') {
-        return `${indent}  ${node.key}: ${stringify(node.value, indentSize)}`;
-      }
-      if (node.status === 'updated') {
-        return `${indent}- ${node.key}: ${stringify(node.oldValue, indentSize)}\n${indent}+ ${node.key}: ${stringify(node.newValue, indentSize)}`;
-      }
-      return `${indent}  ${node.key}: ${iter(node.children, depth + 1)}`;
-    });
-    return [
-      '{',
-      ...lines,
-      `${bracketIndent}}`,
-    ].join('\n');
-  };
+// https://ru.hexlet.io/challenges/js_trees_stringify_exercise
 
-  return iter(value, 1);
+const stringify = (value, depth) => {
+  if (!_.isPlainObject(value)) {
+    return `${value}`;
+  }
+  const keys = _.keys(value);
+  const result = keys.map((key) => {
+    const childValue = value[key];
+    return `${getIndent(depth)}  ${key}: ${stringify(childValue, depth + 1)}\n`;
+  });
+  return `{\n${result.join('')}${getIndent(depth - 1)}  }`;
+};
+
+const getDiffTree = (nodes) => {
+  const iter = (node, depth = 1) => {
+    switch (node.status) {
+      case 'nested':
+        return `\n${getIndent(depth)}  ${node.key}: {${node.children.map((child) => iter(child, depth + 1)).join('')}\n${getIndent(depth)}  }`;
+      case 'updated':
+        return `\n${getIndent(depth)}- ${node.key}: ${stringify(node.removedValue, depth + 1)}\n${getIndent(depth)}+ ${node.key}: ${stringify(node.addedValue, depth + 1)}`;
+      case 'unchanged':
+      case 'added':
+      case 'removed':
+        return `\n${getIndent(depth)}${signs[node.status]}${node.key}: ${stringify(node.value, depth + 1)}`;
+      default:
+        throw new Error(`Type: ${node.status} is unknown.`);
+    }
+  };
+  return iter(nodes);
+};
+
+const stylishOutput = (diffTree) => {
+  const result = diffTree.map((nodes) => getDiffTree(nodes));
+  return `{${result.join('')}\n}`;
 };
 
 export default stylishOutput;
